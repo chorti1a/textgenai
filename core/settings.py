c4940b6890ed344856660ebe0e7291b984faea89
chorti1a <roman.smirnov.166@gmail.com>
2026-04-26 05:01:39 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os  # ← добавьте эту строку
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,6 +108,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static'
 
 CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
