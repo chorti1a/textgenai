9f4586bfbeb37eb2f542f6cb5b4b6e930b344e7b
chorti1a <roman.smirnov.166@gmail.com>
2026-04-30 01:09:34 +0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 from apps.textai import views
 
 urlpatterns = [
+    path('', views.raid_duality_tracker, name='home'),
+    path('api/check/', views.raid_duality_check, name='api_check'),
     path('admin/', admin.site.urls),
-    path('', views.raid_duality_tracker, name='home'),  # Главная страница
     path('textai/', include('apps.textai.urls')),
     path('duality/', include('apps.textai.urls')),
 ]
