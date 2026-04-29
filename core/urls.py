edb43f8df1e5197ad355ac52348e15b58d6895df
chorti1a <roman.smirnov.166@gmail.com>
2026-04-30 00:19:53 +0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # ... другие пути ...
-    path('duality/', include('apps.textai.urls')),
+    path('admin/', admin.site.urls),
+    path('textai/', include('apps.textai.urls')),  # существующие пути textai
+    path('duality/', include('apps.textai.urls')),  # дублируем для короткого URL
 ]
