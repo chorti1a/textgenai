6e6201fe0c4370677d08b5e888e55c42920e8399
chorti1a <roman.smirnov.166@gmail.com>
2026-04-30 00:16:57 +0500

    Update urls.py

--- apps/textai/urls.py
+++ apps/textai/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from apps.textai import views
+from . import views
 
 urlpatterns = [
-    path('', views.textai_view, name='textai'),  # /textai/
-    path('auth/login/', views.bungie_login, name='bungie_login'),  # /textai/auth/login/
-    path('auth/callback/', views.bungie_callback, name='bungie_callback'),
-    path('auth/logout/', views.bungie_logout, name='bungie_logout'),
-    path('auth/status/', views.auth_status, name='auth_status'),
+    path('', views.index, name='textai_index'),
+    path('raid-checker/', views.raid_duality_check, name='raid_duality_check'),
 ]
