1a8de9b36e1d12d80e278385fc9a63b98403ad5b
chorti1a <roman.smirnov.166@gmail.com>
2026-04-30 00:18:06 +0500

    Update urls.py

--- apps/textai/urls.py
+++ apps/textai/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='textai_index'),
-    path('raid-checker/', views.raid_duality_check, name='raid_duality_check'),
+    path('', views.raid_duality_tracker, name='raid_duality_tracker'),
+    path('api/check/', views.raid_duality_check, name='raid_duality_check'),
 ]
