f7a474ce1fb3f0f3cff5e88a12d5b13349ec2954
chorti1a <roman.smirnov.166@gmail.com>
2026-04-30 00:19:07 +0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('textai/', include('apps.textai.urls')),
-    path('', RedirectView.as_view(url='/textai/')),  # редирект с главной на /textai/
+    # ... другие пути ...
+    path('duality/', include('apps.textai.urls')),
 ]
