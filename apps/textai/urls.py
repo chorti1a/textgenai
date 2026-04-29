0f1c6b95b64cf5458feab651be456df7571cd4ce
chorti1a <roman.smirnov.166@gmail.com>
2026-04-30 01:21:23 +0500

    Update urls.py

--- apps/textai/urls.py
+++ apps/textai/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.raid_duality_tracker, name='raid_duality_tracker'),
     path('api/check/', views.raid_duality_check, name='raid_duality_check'),
-    path('raid-checker/', views.raid_duality_check, name='raid_duality_check_alt'),
 ]
